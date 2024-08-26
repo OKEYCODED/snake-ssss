95020ca7cace5327a32d8abe3f6230cfe2b1cb7b
OKEYCODED <okeyrbx@gmail.com>
2024-08-26 16:20:11 +0800

    Update food.js

--- food.js
+++ food.js
@@ -2,7 +2,7 @@ import{ onSnake, expandSnake} from './snake.js'
 import{ randomGridPosition } from '.grid.js'
 
 let food = getRandomFoodPosition()
-const EXPANSION_RATE = 3
+const EXPANSION_RATE = 5
 export funtion update() {
   if (onSanke(food)) {
     expandSnake(EXPANSION_RATE)
